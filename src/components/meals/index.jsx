13474d031ef037785702e937d189b922178db422
hx991024 <sx03861857@outlook.com>
2025-02-02 14:02:33 +0800

    优化meals

--- src/components/meals/index.jsx
+++ src/components/meals/index.jsx
@@ -1,5 +1,6 @@
 import Meal from "./meal";
 import { useState } from "react";
+import styles from "./index.module.css";
 
 const MEAL_LIST = [
   {
@@ -26,11 +27,35 @@ const MEAL_LIST = [
     desc: "汉堡4",
     price: "15",
   },
+  {
+    id: 5,
+    desc: "汉堡5",
+    name: "汉堡5",
+    price: "15",
+  },
+  {
+    id: 6,
+    name: "汉堡6",
+    desc: "汉堡6",
+    price: "15",
+  },
+  {
+    id: 7,
+    name: "汉堡7",
+    desc: "汉堡7",
+    price: "15",
+  },
+  {
+    id: 8,
+    name: "汉堡8",
+    desc: "汉堡8",
+    price: "15",
+  },
 ];
 const index = () => {
   const [meals, setMeals] = useState(MEAL_LIST);
   return (
-    <div>
+    <div className={styles.mealsBox}>
       {meals.map((meal) => (
         <Meal key={meal.id} meal={meal} />
       ))}
